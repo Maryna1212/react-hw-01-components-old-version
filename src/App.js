77f0351acce57fcff30119237b5384fb00a40d47
Maryna Vereshchak <vereschak12@gmail.com>
2021-09-17 21:48:34 +0300

    добавлен компонент transactionhystory

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Statistics from './components/statistics/Statistics';
 import statisticalData from './components/statistics/statistical-data.json';
 import FriendList from './components/friendlist/FriendList';
 import friends from './components/friendlist/friends.json';
+import TransactionHistory from './components/transactions/TransactionHistory';
+import transactions from './components/transactions/transactions.json';
 
 export default function App() {
   return (
@@ -17,6 +19,7 @@ export default function App() {
       />
       <Statistics title="Upload stats" stats={statisticalData} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
 }
